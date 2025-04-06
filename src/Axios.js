61cb575bfdbed92c18eb3b5ff7850087e3c00d30
karem <karemmagdy247@gmail.com>
2025-04-06 20:59:06 +0200

    Update Axios.js

--- src/Axios.js
+++ src/Axios.js
@@ -15,65 +15,53 @@ const axiosInstance = axios.create({
     withCredentials: true,
 });
 
-// Request interceptor
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         return config;
-//     },
-//     (error) => {
-//         console.error('Request error:', error);
-//         return Promise.reject(error);
-//     }
-// );
 
 // Request interceptor
 axiosInstance.interceptors.request.use(
     (config) => {
-      const publicPaths = ["/", "/login", "/register"];
-      const path = new URL(config.baseURL + config.url).pathname;
-  
-      if (publicPaths.includes(path)) {
-        config.withCredentials = false;  
-      } else {
-        config.withCredentials = true;
-      }
-  
-      return config;
+        let fullUrl = config.url.startsWith('http') ? config.url : config.baseURL + config.url;
+        const path = new URL(fullUrl).pathname;
+
+        config.withCredentials = !publicPaths.includes(path);
+        return config;
     },
     (error) => {
-      console.error('Request error:', error);
-      return Promise.reject(error);
+        console.error('Request error:', error);
+        return Promise.reject(error);
     }
-  );
-  
+);
+
 
 // Response interceptor
 axiosInstance.interceptors.response.use(
     (response) => response,
     async (error) => {
         const originalRequest = error.config;
-        
+
         if (!error.response || originalRequest.url.includes('/users/token/refresh/')) {
+            if (!publicPaths.includes(window.location.pathname)) {
+                window.location.href = '/login?session_expired=true';
+            }
             return Promise.reject(error);
         }
 
         // حالة 401 Unauthorized
         if (error.response.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
-            
+
             try {
                 const refreshResponse = await axiosInstance.post(
-                    '/users/token/refresh/', 
-                    {}, 
+                    '/users/token/refresh/',
+                    {},
                     { withCredentials: true }
                 );
-                
+
                 if (refreshResponse.status === 200) {
                     return axiosInstance(originalRequest);
                 }
             } catch (refreshError) {
                 console.error('Refresh token error:', refreshError);
-                
+
                 if (!publicPaths.includes(window.location.pathname)) {
                     window.location.href = '/login?session_expired=true';
                 }
