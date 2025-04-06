7a00b7d6be670b6b18e459f27673a387821cf872
karem <karemmagdy247@gmail.com>
2025-04-06 16:21:12 +0200

    Update Dashboard.js

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -187,7 +187,7 @@ export default function Dashboard(props) {
                     }
                 }
             >
-                <PageContainer className="pb-5">
+                <PageContainer sx={{marginBottom:"50px"}}>
                     <Outlet />
                 </PageContainer>
             </DashboardLayout>
