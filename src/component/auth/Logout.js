c0d62b65c4592522bb3b0adc88c280989c8fe6f9
karem <karemmagdy247@gmail.com>
2025-04-07 15:02:25 +0200

    update

--- src/component/auth/Logout.js
+++ src/component/auth/Logout.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../Axios";
 import { useNavigate } from "react-router-dom";
+import DefaultProgress from "../progress/Default";
 
 export const Logout = () => {
     const [error, setError] = useState(null);
@@ -28,7 +29,7 @@ export const Logout = () => {
             {error ? (
                 <div className="error-message">{error}</div>
             ) : (
-                <div className="logout-message">جاري تسجيل الخروج...</div>
+                <DefaultProgress sx={{width:'100%', height:'100vh', display:'flex'}} />
             )}
         </div>
     );
